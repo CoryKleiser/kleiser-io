19d3d449b2adae35e378bb256d6ab86d19fe8d85
Cory Kleiser <kleisecj@mail.uc.edu>
2018-05-15 15:11:59 -0400

    added structure for work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IntroComponent
+    IntroComponent,
+    WorkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule

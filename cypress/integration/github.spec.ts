7ff39890eecea3a6ee562c48946aab2810b69a61
Cory Kleiser <Cory.Kleiser@gmail.com>
2018-12-03 21:45:24 -0500

    implement one github section test | removed TODO | added semi-colons

--- cypress/integration/github.spec.ts
+++ cypress/integration/github.spec.ts
@@ -4,10 +4,10 @@ describe('Github Section Tests', () => {
     cy.route('GET', 'https://api.stackexchange.com/2.2/users/6713829/top-tags?site=stackoverflow', 'fixture:stackoverflow.topTags.json');
     cy.route('GET', 'https://api.stackexchange.com/2.2/users/6713829/?site=stackoverflow', 'fixture:stackoverflow.userInfo.json');
     cy.route('GET', 'https://api.github.com/users/corykleiser', 'fixture:github.userInfo.json');
-    cy.route('GET', 'https://api.github.com/users/corykleiser/repos', 'fixture:github.userRepos.json')
+    cy.route('GET', 'https://api.github.com/users/corykleiser/repos', 'fixture:github.userRepos.json');
     cy.visit('/');
   });
   it('displays name from user info', () => {
-
+    cy.get('.card-title').contains('Mario');
   });
 });
--- cypress/integration/stackoverflow.spec.ts
+++ cypress/integration/stackoverflow.spec.ts
@@ -4,7 +4,7 @@ describe('StackOverflow Section Tests', () => {
     cy.route('GET', 'https://api.stackexchange.com/2.2/users/6713829/top-tags?site=stackoverflow', 'fixture:stackoverflow.topTags.json');
     cy.route('GET', 'https://api.stackexchange.com/2.2/users/6713829/?site=stackoverflow', 'fixture:stackoverflow.userInfo.json');
     cy.route('GET', 'https://api.github.com/users/corykleiser', 'fixture:github.userInfo.json');
-    cy.route('GET', 'https://api.github.com/users/corykleiser/repos', 'fixture:github.userRepos.json')
+    cy.route('GET', 'https://api.github.com/users/corykleiser/repos', 'fixture:github.userRepos.json');
     cy.visit('/');
   });
   it('displays top tags on load', () => {

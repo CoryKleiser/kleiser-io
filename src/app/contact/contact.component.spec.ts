38639a05c67a5e015960410e0fc4126a3c9640af
Cory Kleiser <Cory.Kleiser@gmail.com>
2018-12-10 19:14:07 -0500

    boiler plate for contact unit test

--- src/app/contact/contact.component.spec.ts
+++ src/app/contact/contact.component.spec.ts
@@ -1,22 +1,35 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { ContactComponent } from './contact.component';
+import {DebugElement} from '@angular/core';
 
 describe('ContactComponent', () => {
   let component: ContactComponent;
   let fixture: ComponentFixture<ContactComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
-    })
-    .compileComponents();
-  }));
+  let nameEl: DebugElement;
+  let emailEl: DebugElement;
+  let subjectEl: DebugElement;
+  let messageEl: DebugElement;
+  let submitEl: DebugElement;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContactComponent ]
+    });
+
+    // create component and test fixtures
     fixture = TestBed.createComponent(ContactComponent);
+
+    // get test component from fixture
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    nameEl = fixture.debugElement.query(By.css('[data-test=contact-name]'));
+    emailEl = fixture.debugElement.query(By.css('[data-test=contact-email]'));
+    subjectEl = fixture.debugElement.query(By.css('[data-test=message-subject]'));
+    messageEl = fixture.debugElement.query(By.css('[data-test=message-body]'));
+    submitEl = fixture.debugElement.query(By.css('[data-test=contact-submit]'));
+
   });
 
   it('should create', () => {

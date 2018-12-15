d5a8724d0fafed5b57b75854ab5071f725cd7f86
Cory Kleiser <Cory.Kleiser@gmail.com>
2018-12-15 13:46:18 -0500

    added ngIf to SO and github sections to wait for data hydration | about test passing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,76 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {WorkComponent} from './work/work.component';
+import {IntroComponent} from './intro/intro.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {NavComponent} from './nav/nav.component';
+import {FormsModule} from '@angular/forms';
+import {StackoverflowService} from './services/stackoverflow.service';
+import {GithubService} from './services/github.service';
+import {EmailService} from './services/email.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 describe('AppComponent', () => {
+  let page: Page;
+  let app: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule, HttpClientTestingModule ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavComponent,
+        IntroComponent,
+        WorkComponent,
+        AboutComponent,
+        ContactComponent
       ],
-    }).compileComponents();
+      providers: [ StackoverflowService, GithubService, EmailService ]
+    });
   }));
+
+  beforeEach(() => {
+    createComponent();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    console.log(app);
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+  it(`should have as title 'Welcome to Kleiser IO'`, async(() => {
+    expect(app.title).toEqual('Welcome to Kleiser IO');
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  /**
+   * This function creates our component and sets up our page
+   */
+  function createComponent() {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    page = new Page();
+
+    // 1st change detection triggers ngOnInit
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    return fixture.whenStable().then(() => {
+      // 2nd change detection displays the async
+      fixture.detectChanges();
+    });
+  }
+
+  // Create Page Class
+  class Page {
+
+    constructor() {}
+
+    //// query helpers ////
+    private query<T>(selector: string): T {
+      return fixture.nativeElement.querySelector(selector);
+    }
+
+    private queryAll<T>(selector: string): T[] {
+      return fixture.nativeElement.querySelectorAll(selector);
+    }
+  }
+
 });

801dd00085dead02f74aba5db9ea534cabfe0a1a
Cory Kleiser <Cory.Kleiser@gmail.com>
2018-12-21 19:57:42 -0500

    removed eye dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { WorkComponent } from './work/work.component';
 import { AboutComponent } from './about/about.component';
 import { StackoverflowService } from './services/stackoverflow.service';
 import { GithubService } from './services/github.service';
-import { EyeComponent } from './eye/eye.component';
 import { ContactComponent } from './contact/contact.component';
 import { EmailService } from './services/email.service';
 import { ForbiddenValidatorDirective } from './shared/forbidden-validator-directive.directive';
@@ -23,7 +22,6 @@ import { NavComponent } from './nav/nav.component';
     IntroComponent,
     WorkComponent,
     AboutComponent,
-    EyeComponent,
     ContactComponent,
     ForbiddenValidatorDirective,
     NavComponent,
